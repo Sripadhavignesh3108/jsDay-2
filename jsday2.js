383aec003cc7c9c9359ccbf0d0fad59a9999b9c5
Sripadhavignesh3108 <sripadhavignesh@gmail.com>
2024-06-27 21:06:03 +0530

    js day 2

---
+++ jsday2.js
@@ -0,0 +1,28 @@
+// for(i=0;i<=15;i++)
+// {
+//     if(i%3==0)
+//     { console.log("fizz")}
+//     else if(i%5==0) 
+//     {console.log("buzz")}
+//     else
+//     {
+//         console.log(i)
+//     }
+// }
+
+
+let a=+prompt();
+
+if(a<=11 && a>=0) console.log("good morning")
+
+else if(a<=15 && a>=12) console.log("good afternoon")
+
+else if(a<=19 && a>=16) console.log("good evening")
+
+else if(a<=23 && a>=20) console.log("good night")
+
+else console.log("wrong time format");
+
+
+
+
